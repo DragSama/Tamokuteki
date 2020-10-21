00b2c0b29ba57729aee974c5ce2a81dd95b82625
DragSama <49992845+DragSama@users.noreply.github.com>
2020-10-21 17:13:42 +0530

    Add ^$ to pattern

--- TamokutekiBot/helpers.py
+++ TamokutekiBot/helpers.py
@@ -2,4 +2,4 @@ from telethon import events
 import re
 
 def command(pattern, outgoing = True):
-    return events.NewMessage(pattern = re.compile("\." + pattern), outgoing = outgoing)
+    return events.NewMessage(pattern = re.compile(f"^\.{pattern}$"), outgoing = outgoing)
