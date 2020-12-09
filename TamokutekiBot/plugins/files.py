7be865295694f210a9f0e6c51c939111a401d990
DragSama <49992845+DragSama@users.noreply.github.com>
2020-12-09 15:23:05 +0530

    Update files.py

--- TamokutekiBot/plugins/files.py
+++ TamokutekiBot/plugins/files.py
@@ -18,7 +18,7 @@
 from TamokutekiBot.helpers import command, format_bytes
 
 
-def progress_message(current, total, _type, event):
+def progress_message(current, total, _type):
     total_size, total_type = format_bytes(total)
     curr_size, curr_type = format_bytes(total)
     return f"{_type} {round(total_size, 2)} {total_type} out of {round(curr_size, 2)} {curr_type}"
